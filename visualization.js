17a229fa9b7d5096fb5fe84c1f0bfb703a58706c
nate <natetatum@icloud.com>
2025-10-18 19:39:36 -0500

    user input and js

--- visualization.js
+++ visualization.js
@@ -1,3 +1,12 @@
+
+isWind = false;
+isSolar = false;
+
+
+wind.style.display = "none"
+solar.style.display = "none"
+
+
 // Wait for the document to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     // Get the chart element
@@ -20,15 +29,74 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Labels:', labels);
         console.log('Values:', values);
     }
-
 });
-sunOrWind.addEventListener("click",function(){
+maxWind = document.getElementsByName("maxWind")[0];
+minWind = document.getElementsByName("minWind")[0];
+preferredWind = document.getElementsByName("preferredWind")[0];
+maxSolar = document.getElementsByName("maxSolar")[0];
+minSolar = document.getElementsByName("minSolar")[0];
+longitude = document.getElementsByName("Longitude")[0];
+latitude = document.getElementsByName("Latitude")[0];
+preferredSolar = document.getElementsByName("preferredSolar")[0];
+maxWind.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+minWind.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+preferredWind.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+maxSolar.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+minSolar.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+preferredSolar.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+longitude.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+latitude.addEventListener("change",function(){
+    // Update the chart or map based on the new input values
+    updateVisualization();
+});
+
+
+updateVisualization = (() =>{
+    MaxWind = maxWind.value;
+    MinWind = minWind.value;
+    PreferredWind = preferredWind.value;
+    MaxSolar = maxSolar.value;
+    MinSolar = minSolar.value;
+    PreferredSolar = preferredSolar.value;
+})
+
+updateInputValue = (() =>{
     const selectedOption = sunOrWind.value;
-    if (selectedOption === "Solar panels") {
+    if (selectedOption == "Solar panels") {
         solar.style.display = "block";
         wind.style.display = "none";
+        isSolar = true;
+        isWind = false;
     } else {
         solar.style.display = "none";
         wind.style.display = "block";
+        isWind = true;
+        isSolar = false;
     }
 })
